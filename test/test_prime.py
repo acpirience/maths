0c4bac654078270fdbda2e1b2ef60728ae15d22a
Acpirience <acpirience@gmail.com>
2026-09-08 12:12:33 +0200

    prime

---
+++ test/test_prime.py
@@ -0,0 +1,20 @@
+from prime.prime import MAX_PRIME, PRIME_LIST, is_prime
+
+
+def test_is_prime():
+    assert is_prime(2) == True
+    assert is_prime(3) == True
+    assert is_prime(4) == False
+    assert is_prime(17) == True
+    assert is_prime(25) == False
+
+
+def test_is_prime_in_prime_list():
+    for prime in PRIME_LIST:
+        assert is_prime(prime) == True
+
+
+def test_is_prime_not_in_prime_list():
+    for i in range(1, MAX_PRIME + 1):
+        if i not in PRIME_LIST:
+            assert is_prime(i) == False
